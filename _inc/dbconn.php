b959c3100de666a317ea64b1c01db0ab67ee8ed3
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-22 17:05:03 +0100

    Update dbconn.php

--- _inc/dbconn.php
+++ _inc/dbconn.php
@@ -3,6 +3,12 @@ $serverName="eu-cdbr-west-03.cleardb.net";
 $dbusername="b3c09316299ed3";
 $dbpassword="d8615c54";
 $dbname="heroku_593953e5dc152a6";
-mysql_connect($serverName,$dbusername,$dbpassword)/* or die('the website is down for maintainance')*/;
-mysql_select_db($dbname) or die(mysql_error());
+
+$mysql = new mysqli($serverName,$dbusername,$dbpassword, $dbname);
+
+if($mysql->connect_errno)
+{
+	echo "Failed to connect to MYSQL: ".$mysql->connect_error;
+}
+ $mysql->select_db($dbname);
 ?>
