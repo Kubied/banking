c3e105a397b74ea6266779c0cc084888dbdb019a
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-22 16:37:42 +0100

    Update dbconn.php

--- _inc/dbconn.php
+++ _inc/dbconn.php
@@ -1,8 +1,8 @@
 <?php
-$serverName="localhost";
-$dbusername="root";
-$dbpassword="dexde3";
-$dbname="bank_db";
+$serverName="eu-cdbr-west-03.cleardb.net";
+$dbusername="b3c09316299ed3";
+$dbpassword="d8615c54";
+$dbname="heroku_593953e5dc152a6";
 mysql_connect($serverName,$dbusername,$dbpassword)/* or die('the website is down for maintainance')*/;
 mysql_select_db($dbname) or die(mysql_error());
 ?>
