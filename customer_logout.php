6a82e36e7f46f0f156e2497fd2d6095f67f45e64
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-22 17:13:49 +0100

    Update customer_logout.php

--- customer_logout.php
+++ customer_logout.php
@@ -6,7 +6,7 @@ include '_inc/dbconn.php';
 $date=date('Y-m-d h:i:s');
 $id=$_SESSION['login_id'];
 $sql="UPDATE customer SET lastlogin='$date' WHERE id='$id'";
-mysql_query($sql) or die(mysql_error());
+$mysql->query($sql) or die($mysql->error());
 
 session_destroy();
 header('location:index.php');
