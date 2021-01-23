5a61690187a0bb28e2afa9ebe1df3763535327a2
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-23 22:44:07 +0100

    Update classes.php

--- classes.php
+++ classes.php
@@ -153,7 +153,7 @@ class Account_Summary
 }
 	public function showCustomerSummary()
 	{
-	   return '<div class="customer_body">
+	   return echo'<div class="customer_body">
                 <div class="content1">
             <p><span class="heading">Account No: </span><?php echo $this->acc_no;?></p>
             <p><span class="heading">Branch: </span><?php echo $this->branch;?></p>
