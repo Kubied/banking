92cc4ee688c41963b93bb872f2cc0c4873222ad7
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-23 14:09:10 +0100

    Update add_staff.php

--- add_staff.php
+++ add_staff.php
@@ -5,20 +5,24 @@ if(!isset($_SESSION['admin_login']))
     header('location:adminlogin.php');   
 ?>
 <?php
+include 'classes.php';
 include '_inc/dbconn.php';
-$name=  $mysql->real_escape_string($_REQUEST['staff_name']);
-$gender=  $mysql->real_escape_string($_REQUEST['staff_gender']);
-$dob=  $mysql->real_escape_string($_REQUEST['staff_dob']);
-$status=  $mysql->real_escape_string($_REQUEST['staff_status']);
-$dept=  $mysql->real_escape_string($_REQUEST['staff_dept']);
-$doj=  $mysql->real_escape_string($_REQUEST['staff_doj']);
-$address=  $mysql->real_escape_string($_REQUEST['staff_address']);
-$mobile=  $mysql->real_escape_string($_REQUEST['staff_mobile']);
-$email= $mysql->real_escape_string($_REQUEST['staff_email']);
-$password=  $mysql->real_escape_string($_REQUEST['staff_pwd']);
 
-$sql="insert into staff values('','$name','$dob','$status','$dept','$doj','$address','$mobile',
-    '$email','$password','$gender','')";
+$staff = new Staff();
+
+$staff->setName($mysql->real_escape_string($_REQUEST['staff_name']));
+$staff->setGender($mysql->real_escape_string($_REQUEST['staff_gender']));
+$staff->setDob($mysql->real_escape_string($_REQUEST['staff_dob']));
+$staff->setAccstatus($mysql->real_escape_string($_REQUEST['staff_status']));
+$staff->setDept($mysql->real_escape_string($_REQUEST['staff_dept']));
+$staff->setDoj($mysql->real_escape_string($_REQUEST['staff_doj']));
+$staff->setAddress($mysql->real_escape_string($_REQUEST['staff_address']));
+$staff->setMobile($mysql->real_escape_string($_REQUEST['staff_mobile']));
+$staff->setEmail($mysql->real_escape_string($_REQUEST['staff_email']));
+$staff->setPassword($mysql->real_escape_string($_REQUEST['staff_pwd']));
+
+$sql="insert into staff values('','".$staff->getName()."','".$staff->getDob()."','".$staff->getAccstatus()."','".$staff->getdept()."','".$staff->getDoj()."','".$staff->getAddress()."','".$staff->getMobile()."',
+    '".$staff->getEmail()."','$".$staff->getPassword()."','".$staff->getGender()."','')";
 $mysql->query($sql) or die("the email-id is already registered");
 header('location:admin_hompage.php');
 ?>
