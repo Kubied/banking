d7ff3cbf38cfe1b76c4b64f579cd36f998e01bca
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-23 22:42:44 +0100

    Update customer_account_summary.php

--- customer_account_summary.php
+++ customer_account_summary.php
@@ -64,7 +64,7 @@ if(!isset($_SESSION['customer_login']))
             <div class="customer_body">
                 <div class="content1">
             <p><span class="heading">Account No: </span><?php echo $accsum->getAccno();?></p>
-            <p><span class="heading">Branch: </span><?php echo $this->branch;?></p>
+            <p><span class="heading">Branch: </span><?php echo $accsum->getBranch();?></p>
             <p><span class="heading">Branch Code: </span><?php echo $this->branch_code;?></p>
             </div>
             
