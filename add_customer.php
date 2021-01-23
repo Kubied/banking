c5f0ffca9750f864545d742cb177e4af7a70a204
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-23 13:38:29 +0100

    Update add_customer.php

--- add_customer.php
+++ add_customer.php
@@ -6,21 +6,24 @@ if(!isset($_SESSION['admin_login']))
 ?>
     <?php
 include '_inc/dbconn.php';
-$name=  $mysql->real_escape_string($_REQUEST['customer_name']);
-$gender=  $mysql->real_escape_string($_REQUEST['customer_gender']);
-$dob=  $mysql->escape_string($_REQUEST['customer_dob']);
-$nominee=  $mysql->real_escape_string($_REQUEST['customer_nominee']);
-$type=  $mysql->real_escape_string($_REQUEST['customer_account']);
-$credit=  $mysql->real_escape_string($_REQUEST['initial']);
-$address=  $mysql->real_escape_string($_REQUEST['customer_address']);
-$mobile=  $mysql->real_escape_string($_REQUEST['customer_mobile']);
-$email= $mysql->real_escape_string($_REQUEST['customer_email']);
+include 'classes.php';
 
-$password= mysql_real_escape_string($_REQUEST['customer_pwd']);
+$cust = new Customer();
 
-$branch=  mysql_real_escape_string($_REQUEST['branch']);
-$date=date("Y-m-d");
-switch($branch){
+$cust->setName($mysql->real_escape_string($_REQUEST['customer_name']));
+$cust->setGender($mysql->real_escape_string($_REQUEST['customer_gender']));
+$cust->setDob($mysql->escape_string($_REQUEST['customer_dob']);
+$cust->setNominee($mysql->real_escape_string($_REQUEST['customer_nominee']));
+$cust->setAcctype($mysql->real_escape_string($_REQUEST['customer_account']));
+$cust->setCredit($mysql->real_escape_string($_REQUEST['initial']));
+$cust->setAddress($mysql->real_escape_string($_REQUEST['customer_address']));
+$cust->setMobile($mysql->real_escape_string($_REQUEST['customer_mobile']));
+$cust->setEmail($mysql->real_escape_string($_REQUEST['customer_email']));
+$cust->setPassword(mysql_real_escape_string($_REQUEST['customer_pwd']));
+$cust->setBranch(mysql_real_escape_string($_REQUEST['branch']));
+$cust->setDate(date("Y-m-d"));
+
+              switch($cust->getBranch()){
 case 'Tomek': $ifsc="K421A";
     break;
 case 'Romek': $ifsc="D30AC";
@@ -37,11 +40,11 @@ $sql1="CREATE TABLE passbook".$id."
     (transactionid int(5) AUTO_INCREMENT, transactiondate date, name VARCHAR(255), branch VARCHAR(255), ifsc VARCHAR(255), credit int(10), debit int(10), 
     amount float(10,2), narration VARCHAR(255), PRIMARY KEY (transactionid))";
 
-$sql="insert into customer values('','$name','$gender','$dob','$nominee','$type','$address','$mobile',
-    '$email','$password','$branch','$ifsc','','ACTIVE')";
-$mysql->query($sql) or die("Email already exists!");
+$sql="insert into customer values('','".$cust->getName()."','".$cust->getGender()."','".$cust->getDob()."','".$cust->getNominee()."','".$cust->getAcctype()."','".$cust->getAddress()."','".$cust->getMobile()."',
+    '".$cust->getEmail()."','".$cust->getPassword()."','".$cust->getBranch()."','".$cust->getIfsc()."','','ACTIVE')";
+$mysql->query($sqll) or die("Email already exists!");
 $mysql->query($sql1) or die($mysql->error());
-$sql4="insert into passbook".$id." values('','$date','$name','$branch','$ifsc','$credit','0','$credit','Account Open')";
+$sql4="insert into passbook".$id." values('','".$cust->getDate()."','".$cust->getName()."','".$cust->getBranch()."','".$cust->getIfsc()."','".$cust->getCredit()."','0','".$cust->getCredit()."','Account Open')";
 $mysql->query($sql4) or die($mysql->error());
 header('location:admin_hompage.php');
 ?>
