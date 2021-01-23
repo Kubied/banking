0e1f399c720beb416e5678d762f35ac3a05398c5
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-23 07:45:20 +0100

    Update add_staff.php

--- add_staff.php
+++ add_staff.php
@@ -6,16 +6,16 @@ if(!isset($_SESSION['admin_login']))
 ?>
 <?php
 include '_inc/dbconn.php';
-$name=  mysql_real_escape_string($_REQUEST['staff_name']);
-$gender=  mysql_real_escape_string($_REQUEST['staff_gender']);
-$dob=  mysql_real_escape_string($_REQUEST['staff_dob']);
-$status=  mysql_real_escape_string($_REQUEST['staff_status']);
-$dept=  mysql_real_escape_string($_REQUEST['staff_dept']);
-$doj=  mysql_real_escape_string($_REQUEST['staff_doj']);
-$address=  mysql_real_escape_string($_REQUEST['staff_address']);
-$mobile=  mysql_real_escape_string($_REQUEST['staff_mobile']);
-$email= mysql_real_escape_string($_REQUEST['staff_email']);
-$password=  mysql_real_escape_string($_REQUEST['staff_pwd']);
+$name=  $mysql->real_escape_string($_REQUEST['staff_name']);
+$gender=  $mysql->real_escape_string($_REQUEST['staff_gender']);
+$dob=  $mysql->real_escape_string($_REQUEST['staff_dob']);
+$status=  $mysql->real_escape_string($_REQUEST['staff_status']);
+$dept=  $mysql->real_escape_string($_REQUEST['staff_dept']);
+$doj=  $mysql->real_escape_string($_REQUEST['staff_doj']);
+$address=  $mysql->real_escape_string($_REQUEST['staff_address']);
+$mobile=  $mysql->real_escape_string($_REQUEST['staff_mobile']);
+$email= $mysql->real_escape_string($_REQUEST['staff_email']);
+$password=  $mysql->real_escape_string($_REQUEST['staff_pwd']);
 
 $sql="insert into staff values('','$name','$dob','$status','$dept','$doj','$address','$mobile',
     '$email','$password','$gender','')";
