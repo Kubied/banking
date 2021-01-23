ba476894caa8abcf9a962a0da2c8f48d12e5e620
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-23 14:34:05 +0100

    Update alter_staff.php

--- alter_staff.php
+++ alter_staff.php
@@ -6,19 +6,23 @@ if(!isset($_SESSION['admin_login']))
 ?>
 <?php
 include '_inc/dbconn.php';
-$name=  $mysql->real_escape_string($_REQUEST['edit_name']);
-$gender=  $mysql->real_escape_string($_REQUEST['edit_gender']);
-$dob=  $mysql->real_escape_string($_REQUEST['edit_dob']);
-$id=  $mysql->real_escape_string($_REQUEST['current_id']);
-$status= $mysql->real_escape_string($_REQUEST['edit_status']);
-$dept=  $mysql->real_escape_string($_REQUEST['edit_dept']);
-$doj=  $mysql->real_escape_string($_REQUEST['edit_doj']);
-$address=  $mysql->real_escape_string($_REQUEST['edit_address']);
-$mobile=  $mysql->real_escape_string($_REQUEST['edit_mobile']);
+include 'classes.php';
 
-$sql="UPDATE staff SET  name='$name', dob='$dob', relationship='$status', 
-    department='$dept', doj='$doj', address='$address', 
-        mobile='$mobile', gender='$gender' WHERE id='$id'";
+$staff = new Staff();
+
+$staff->setName($mysql->real_escape_string($_REQUEST['edit_name']));
+$staff->setGender($mysql->real_escape_string($_REQUEST['edit_gender']));
+$staff->setDob($mysql->real_escape_string($_REQUEST['edit_dob']));
+$staff->setId($mysql->real_escape_string($_REQUEST['current_id']));
+$staff->Accstatus($mysql->real_escape_string($_REQUEST['edit_status']));
+$staff->setDept($mysql->real_escape_string($_REQUEST['edit_dept']));
+$staff->setDoj($mysql->real_escape_string($_REQUEST['edit_doj']));
+$staff->setAddress($mysql->real_escape_string($_REQUEST['edit_address']));
+$staff->setMobile($mysql->real_escape_string($_REQUEST['edit_mobile']));
+
+$sql="UPDATE staff SET  name='".$staff->getName()."', dob='".$staff->getDob()."', relationship='".$staff->getAccstatus()."', 
+    department='".$staff->getDept()."', doj='".$staff->getDoj()."', address='".$staff->getAddress()."', 
+        mobile='".$staff->getMobile()."', gender='".$staff->getGender()."' WHERE id='".$staff->getId()."'";
 $mysql->query($sql) or die($mysql->error());
 header('location:admin_hompage.php');
 ?>
