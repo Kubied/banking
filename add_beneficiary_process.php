e3aacf65188423fcd8132069db9906fb51421b76
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-23 12:48:41 +0100

    Update add_beneficiary_process.php

--- add_beneficiary_process.php
+++ add_beneficiary_process.php
@@ -5,41 +5,45 @@ if(!isset($_SESSION['customer_login']))
     header('location:index.php');   
 ?>
 <?php
-                $sender_id=$_SESSION["login_id"];
-                $sender_name=$_SESSION["name"];
+include 'classes.php';
+
+        $benef = new Beneficiary();
+
+                $benef->setSenderid($_SESSION["login_id"]);
+                $benef->setSendername($_SESSION["name"]);
                 
-                $Payee_name=$_REQUEST['name'];
-                $acc_no=$_REQUEST['account_no'];
-                $branch=$_REQUEST['branch_select'];
-                $ifsc=$_REQUEST['ifsc_code'];
+                $benef->setPayeename($_REQUEST['name']);
+                $benef->setAccno($_REQUEST['account_no']);
+                $benef->setBranch($_REQUEST['branch_select']);
+                $benef->setIfsc($_REQUEST['ifsc_code']);
                 
                 
                 include '_inc/dbconn.php';
-                $sql1="SELECT * FROM beneficiary1 WHERE sender_id='$sender_id' AND reciever_id='$acc_no'";
+                $sql1="SELECT * FROM beneficiary1 WHERE sender_id='$sender_id' AND reciever_id='".$benef->getAccno()."'";
                 $result1=  $mysql->query($sql1);
                 $rws1=  $result1->fetch_array();
-                $s1=$rws1[1];
-                $s2=$rws1[3];
+                $benef->setS1($rws1[1]);
+                $benef->setS2($rws1[3]);
                 
                 
-                $sql="SELECT * FROM customer WHERE id='$acc_no'";
+                $sql="SELECT * FROM customer WHERE id='".$benef->getAccno()."'";
                 
                 $result=  $mysql->query($sql) or die($mysql->error());
                 $rws=  $result->fetch_array();
                 
-                if($sender_id==$rws[0]) //can't send request to himself
+                if($benef->getSenderid() == $rws[0]) //can't send request to himself
                 {
                 echo '<script>alert("You cant add yourself as a beneficiery!");';
                      echo 'window.location= "add_beneficiary.php";</script>';
                 }
                 
-                elseif($s1==$sender_id && $s2==$acc_no)
+                elseif($benef->getS1() == $benef->getSenderid() && $benef->getS2() == $benef->getAccno())
                 {
                     echo '<script>alert("You cant add a beneficiery twice!");';
                      echo 'window.location= "add_beneficiary.php";</script>';
                 }
                 
-                elseif($rws[1]!=$Payee_name && $rws[0]!=$acc_no && $rws[11]!=$branch && $rws[12]!=$ifsc)
+                elseif($rws[1]!=$benef->getPayeename() && $rws[0]!=$benef->getAccno() && $rws[11]!=$benef->getBranch() && $rws[12]!=$benef->getIfsc())
                 {
                 echo '<script>alert("Beneficiary Not Found!\nPlease enter correct details");';
                      echo 'window.location= "add_beneficiary.php";</script>';
@@ -48,8 +52,8 @@ if(!isset($_SESSION['customer_login']))
                 
                 else{
                      
-                $status="PENDING";
-                $sql="insert into beneficiary1 values('','$sender_id','$sender_name','$acc_no','$Payee_name','$status')";
+                $benef->setStatus("PENDING");
+                $sql="insert into beneficiary1 values('','".$benef->getSenderid()."','".$benef->getSendername()."','".$benef->getAccno()."','".$benef->getPayeename()."','".$benef->getStatus()."')";
                 $mysql->query($sql) or die($mysql->error());
                 header("location:display_beneficiary.php");
                 }
