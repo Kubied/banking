8e7581cae231099a86099ab27d3539257be10215
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-23 14:27:21 +0100

    Update alter_customer.php

--- alter_customer.php
+++ alter_customer.php
@@ -7,18 +7,20 @@ if(!isset($_SESSION['admin_login']))
 
 <?php
 include '_inc/dbconn.php';
-$name=  $mysql->real_escape_string($_REQUEST['edit_name']);
-$gender=  $mysql->real_escape_string($_REQUEST['edit_gender']);
-$dob=  $mysql->real_escape_string($_REQUEST['edit_dob']);
-$id=  $mysql->real_escape_string($_REQUEST['current_id']);
-$type=  $mysql->real_escape_string($_REQUEST['edit_account']);
-$nominee=  $mysql->real_escape_string($_REQUEST['edit_nominee']);
-$address=  $mysql->real_escape_string($_REQUEST['edit_address']);
-$mobile=  $mysql->real_escape_string($_REQUEST['edit_mobile']);
+include 'classes.php';
+$cust = new Customer();
 
-$sql="UPDATE customer SET  name='$name', dob='$dob', nominee='$nominee', account='$type', 
-     address='$address', 
-        mobile='$mobile', gender='$gender' WHERE id='$id'";
+$cust->setName($mysql->real_escape_string($_REQUEST['edit_name']));
+$cust->setGender($mysql->real_escape_string($_REQUEST['edit_gender']));
+$cust->setDob($mysql->real_escape_string($_REQUEST['edit_dob']));
+$cust->setId($mysql->real_escape_string($_REQUEST['current_id']));
+$cust->setAcctype($mysql->real_escape_string($_REQUEST['edit_account']));
+$cust->setNominee($mysql->real_escape_string($_REQUEST['edit_nominee']));
+$cust->setAddress($mysql->real_escape_string($_REQUEST['edit_address']));
+$cust->setMobile($mysql->real_escape_string($_REQUEST['edit_mobile']));
+
+$sql="UPDATE customer SET  name='".$cust->getName()."', dob='".$cust->getDob()."', nominee='".$cust->getNominee()."', account='".$cust->getAcctype()."', 
+     address='".$cust->getAddress()."', mobile='".$cust->getMobile()."', gender='".$cust->getGender()."' WHERE id='".$cust->getId()."'";
 $mysql->query($sql) or die($mysql->error());
 header('location:admin_hompage.php');
 ?>
