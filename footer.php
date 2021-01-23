1d99cf1f86878aa8b68f69de16d45bfaabcdbde1
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-23 11:33:26 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,19 +1,10 @@
 <div class="footer">
 <ul>
                    
-                   <div class="footer_content">
-                   <li><a href="features.php">Features </a></li>
-                   <li><a href="contact.php"> Contact</a></li>
-                 
-                   <li><a href="safeonlinebanking.php">Safe online Banking tips</a></li>
-                   
-                   <li style="padding-left:450px;">Copyright@onlinebanking.com 2014</li>
-                   
+                   <div class="footer_content">                           
+                   <li style="padding-left:450px;">Copyright@2021</li>                 
                    </div>
-                   </ul>                    
-       
-    
-    
+                   </ul>                                
  </div>
            </div> 
     </body>
