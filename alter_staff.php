4817727727b6df358e4a4ae12abba3bb038668e5
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-23 08:10:36 +0100

    Update alter_staff.php

--- alter_staff.php
+++ alter_staff.php
@@ -6,19 +6,19 @@ if(!isset($_SESSION['admin_login']))
 ?>
 <?php
 include '_inc/dbconn.php';
-$name=  mysql_real_escape_string($_REQUEST['edit_name']);
-$gender=  mysql_real_escape_string($_REQUEST['edit_gender']);
-$dob=  mysql_real_escape_string($_REQUEST['edit_dob']);
-$id=  mysql_real_escape_string($_REQUEST['current_id']);
-$status=  mysql_real_escape_string($_REQUEST['edit_status']);
-$dept=  mysql_real_escape_string($_REQUEST['edit_dept']);
-$doj=  mysql_real_escape_string($_REQUEST['edit_doj']);
-$address=  mysql_real_escape_string($_REQUEST['edit_address']);
-$mobile=  mysql_real_escape_string($_REQUEST['edit_mobile']);
+$name=  $mysql->real_escape_string($_REQUEST['edit_name']);
+$gender=  $mysql->real_escape_string($_REQUEST['edit_gender']);
+$dob=  $mysql->real_escape_string($_REQUEST['edit_dob']);
+$id=  $mysql->real_escape_string($_REQUEST['current_id']);
+$status= $mysql->real_escape_string($_REQUEST['edit_status']);
+$dept=  $mysql->real_escape_string($_REQUEST['edit_dept']);
+$doj=  $mysql->real_escape_string($_REQUEST['edit_doj']);
+$address=  $mysql->real_escape_string($_REQUEST['edit_address']);
+$mobile=  $mysql->real_escape_string($_REQUEST['edit_mobile']);
 
 $sql="UPDATE staff SET  name='$name', dob='$dob', relationship='$status', 
     department='$dept', doj='$doj', address='$address', 
         mobile='$mobile', gender='$gender' WHERE id='$id'";
-mysql_query($sql) or die(mysql_error());
+$mysql->query($sql) or die($mysql->error());
 header('location:admin_hompage.php');
 ?>
