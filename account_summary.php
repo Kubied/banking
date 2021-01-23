ef19f2032f2cdf733d5d8b3792171dae7bebd072
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-23 12:08:14 +0100

    Update account_summary.php

--- account_summary.php
+++ account_summary.php
@@ -18,6 +18,10 @@ if(!isset($_SESSION['customer_login']))
            <?php include 'customer_navbar.php'?>
             
             <?php
+        include 'classes.php';
+        
+             $acc_sum = new Account_Summary();
+        
                 $cust_id=$_SESSION['cust_id'];
                 include '_inc/dbconn.php';
                 $sql="SELECT * FROM customer WHERE email='$cust_id'";
@@ -25,42 +29,42 @@ if(!isset($_SESSION['customer_login']))
                 $rws=  $result->fetch_array();
                 
                 
-                $name= $rws[1];
-                $account_no= $rws[0];
-                $branch=$rws[10];
-                $branch_code= $rws[11];
-                $last_login= $rws[12];
-                $acc_status=$rws[13];
-                $address=$rws[6];
-                $acc_type=$rws[5];
+                $acc_sum->setName($rws[1]);
+                $acc_sum->setAccno($rws[0]);
+                $acc_sum->setBranch($rws[10]);
+                $acc_sum->setBranchcode($rws[11]);
+                $acc_sum->setLastlogin($rws[12]);
+                $acc_sum->setAccstatus($rws[13]);
+                $acc_sum->setAddress($rws[6]);
+                $acc_sum->setAcctype($rws[5]);
                 
-                $gender=$rws[2];
-                $mobile=$rws[7];
-                $email=$rws[8];
+                $acc_sum->setGender($rws[2]);
+                $acc_sum->setMobile($rws[7]);
+                $acc_sum->setEmail($rws[8]);
                 
-                $_SESSION['login_id']=$account_no;
-                $_SESSION['name']=$name;
+                $_SESSION['login_id']=$acc_sum->getAccno();
+                $_SESSION['name']=$acc_sum->getName();
                 
                 $sql="SELECT * FROM passbook".$_SESSION['login_id'] ;
                 $result=  $mysql_query($sql) or die($mysql->error());
                 $rws= $result->fetch_array();
                 
-                $balance=$rws[6];
+                $acc_sum->setBalance($rws[6]);
                                 
 ?>
-            <p>Name: <?php echo $name;?></p>
-            <p>gender: <?php if($gender=='M') echo "Male"; else echo "Female";?></p>
-            <p>Mobile: <?php echo $mobile;?></p>
-            <p>Email: <?php echo $email;?></p>
+            <p>Name: <?php echo $acc_sum->getName();?></p>
+            <p>gender: <?php if($acc_sum->getGender()=='M') echo "Male"; else echo "Female";?></p>
+            <p>Mobile: <?php echo $acc_sum->getMobile;?></p>
+            <p>Email: <?php echo $acc_sum->getEmail;?></p>
             <br>
-            <p>Account No: <?php echo $account_no;?></p>
-            <p>Branch: <?php echo $branch;?></p>
-            <p>Branch Code: <?php echo $branch_code;?></p>
-            <p>Last Login: <?php echo $last_login;?></p>
+            <p>Account No: <?php echo $acc_sum->getAccno();?></p>
+            <p>Branch: <?php echo $acc_sum->getBranch();?></p>
+            <p>Branch Code: <?php echo $acc_sum->getBranchcode();?></p>
+            <p>Last Login: <?php echo $acc_sum->getLastlogin();?></p>
             <br>
-            <p>Account status: <?php echo $acc_status;?></p>
-            <p>Account Type: <?php echo $acc_type;?></p>
-            <p>Address: <?php echo $address;?></p>
+            <p>Account status: <?php echo $acc_sum->getAccstatus();?></p>
+            <p>Account Type: <?php echo $acc_sum->getAcctype();?></p>
+            <p>Address: <?php echo $acc_sum->getAddress();?></p>
             
         </div>
                <?php include 'footer.php';?>
