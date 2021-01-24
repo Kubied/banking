aa5e19f7fab533689a29b0a98d79d46413e973d6
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-24 10:54:35 +0100

    Update customer_issue_atm_process.php

--- customer_issue_atm_process.php
+++ customer_issue_atm_process.php
@@ -6,45 +6,51 @@ if(!isset($_SESSION['customer_login']))
 ?>
 <?php
 include '_inc/dbconn.php';
-$name=$_SESSION['name'];
-$account_no=$_SESSION['login_id'];
-$option=$_REQUEST['atm'];
+include 'classes.php';
 
-$atm_status=$cheque_book_status="NOT REQUESTED";
-if($option=='ATM')
-    $atm_status="PENDING";
+$issue = new Customer_Issue();
+
+$issue->setName($_SESSION['name']);
+$issue->setSenderid($_SESSION['login_id']);
+$issue->setOption($_REQUEST['atm']);
+
+$issue->setAtmstatus("NOT REQUESTED");
+$issue->setChequestatus("NOT REQUESTED");
+
+if($issue->getOption() == 'ATM')
+    $issue->setAtmstatus("PENDING");
 else
-    $cheque_book_status="PENDING";
+    $issue->setChequestatus("PENDING");
     
 
-    $sql="SELECT * FROM cheque_book WHERE account_no='$account_no'";
+    $sql="SELECT * FROM cheque_book WHERE account_no='".$issue->getSenderid()."'";
     $result=$mysql->query($sql) or die($mysql->error());
     $rws=$result->fetch_array();
-    $c_status=$rws[3];
-    $c_id=$rws[2];
+    $issue->setChequestatus($rws[3]);
+    $issue->setChequeid($rws[2]);
     
-    $sql="SELECT * FROM atm WHERE account_no='$account_no'";
+    $sql="SELECT * FROM atm WHERE account_no='".$issue->getSenderid()."'";
     $result=  $mysql->query($sql) or die($mysql->error());
     $rws=  $result->fetch_array();
-    $a_status=$rws[3];
-    $a_id=$rws[2];
+    $issue->setAtmstatus($rws[3]);
+    $issue->setAtmid($rws[2]);
     
     
-    if(($option=='ATM' && (($a_status=='PENDING')||($a_status=='ISSUED'))) || ($option=='CHEQUE' && (($c_status=='PENDING')||($c_status=='ISSUED'))))           
+    if(($issue->getOption() == 'ATM' && (($issue->getAtmstatus() == 'PENDING') || ($issue->getAtmstatus() == 'ISSUED'))) || ($issue->getOption() == 'CHEQUE' && (($issue->getChequestatus() == 'PENDING')||($issue->getChequestatus() == 'ISSUED'))))           
     {
         echo '<script>alert("You have already made a request!");';
    echo 'window.location= "customer_issue_atm.php";</script>';
 }
   
-elseif($option=='ATM'){
-$sql="insert into atm values('','$name','$account_no','$atm_status')";
+elseif($issue->getOption() == 'ATM'){
+$sql="insert into atm values('','".$issue->getName()."','".$issue->getSenderid()."','".$issue->getAtmstatus()."')";
 $mysql->query($sql) or die($mysql->error());
 
 echo '<script>alert("Request succesfull. You will recieve confirmation from branch very soon.");';
 echo 'window.location= "customer_issue_atm.php";</script>';
 }
 else {
-$sql="insert into cheque_book values('','$name','$account_no','$cheque_book_status')";
+$sql="insert into cheque_book values('','".$issue->getName()."','".$issue->getSenderid()."','".$issue->getChequestatus()."')";
 $mysql->query($sql) or die($mysql->error());
 
 echo '<script>alert("Request succesfull. You will recieve confirmation from branch very soon.");';
