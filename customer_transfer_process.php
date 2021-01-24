393ffacdf1bfe52516ab81b063b1a0d490a1055e
Kubied <77802486+Kubied@users.noreply.github.com>
2021-01-24 08:21:27 +0100

    Update customer_transfer_process.php

--- customer_transfer_process.php
+++ customer_transfer_process.php
@@ -3,55 +3,57 @@ session_start();
         
 if(!isset($_SESSION['customer_login'])) 
     header('location:index.php');   
-?>
-<?php
-     $t_amount=$_REQUEST['t_val'];
-     $sender_id=$_SESSION["login_id"];
-     $reciever_id=$_REQUEST['transfer'];
+
+include 'classes.php';
+$trans = new Transaction();
+
+     $trans->setAmount_t($_REQUEST['t_val']);
+     $trans->setSenderid($_SESSION["login_id"]);
+     $trans->setRecieverid($_REQUEST['transfer']);
      
      //select last transaction id in reciever's passbook.
      include '_inc/dbconn.php';
-     $sql="SELECT MAX(transactionid) from passbook".$reciever_id;
+     $sql="SELECT MAX(transactionid) from passbook".$trans->getRecieverid();
      $result=$mysql->query($sql) or die($mysql->error());
      $rws=  $result->fetch_array();
      $r_last_tid=$rws[0];
     
     //select the details in the last row of reciever's passbook.
-    $sql="SELECT * from passbook".$reciever_id." WHERE transactionid='$r_last_tid'";
+    $sql="SELECT * from passbook".$trans->getRecieverid." WHERE transactionid='$r_last_tid'";
     $result=$mysql->query($sql) or die($mysql->error());
     while($rws= $result->fetch_array()){
-    $r_amount=$rws[7];
-    $r_name=$rws[2];
-    $r_branch=$rws[3];
-    $r_ifsc=$rws[4];
+    $trans->setAmount_r($rws[7]);
+    $trans->setName_r($rws[2]);
+    $trans->setBranch_r($rws[3]);
+    $trans->setIfsc_r($rws[4]);
     }
     
    //select the last transaction id in the sender's passbook
-     $sql="SELECT MAX(transactionid) from passbook".$sender_id;
+     $sql="SELECT MAX(transactionid) from passbook".$trans->getSenderid();
      $result=$mysql->query($sql) or die($mysql->error());
      $rws=  $result->fetch_array();
      $s_last_tid=$rws[0];
     
     //select the details in the last row of sender's passbook.
-    $sql="SELECT * from passbook".$sender_id." WHERE transactionid='$s_last_tid'";
+    $sql="SELECT * from passbook".$trans->getSenderid()." WHERE transactionid='$s_last_tid'";
     $result=$mysql->query($sql) or die($mysql->error());
     while($rws= $result->fetch_array()) {
-    $s_amount=$rws[7];
-    $s_name=$rws[2];
-    $s_branch=$rws[3];
-    $s_ifsc=$rws[4];
+    $trans->setAmount_s($rws[7]);
+    $trans->setName_s($rws[2]);
+    $trans->setBranch_s($rws[3]);
+    $trans->setIfsc_s($rws[4]);
     }
     
-    $date=date("Y-m-d");
+    $trans->setDate(date("Y-m-d"));
     
-    $s_total=$s_amount-$t_amount; //sender's final balance.
+    $s_total=$trans->getAmount_s() - $trans->getAmount_t(); //sender's final balance.
     
-    if($s_amount<=500)
+    if($trans->getAmount_s() <= 500)
     {
         echo '<script>alert("Your account balance is less than Rs. 500.\n\nYou must maintain a minimum balance of Rs. 500 in order to proceed with the transfer.");';
         echo 'window.location= "customer_transfer.php";</script>';
     }
-    elseif($t_amount<100){
+    elseif($trans->getAmount_t() < 100){
          echo '<script>alert("You cannot transfer less than Rs. 100");';
         echo 'window.location= "customer_transfer.php";</script>';
     }
@@ -63,13 +65,13 @@ if(!isset($_SESSION['customer_login']))
     
     else{ 
         //insert statement into reciever passbook.
-        $r_total=$r_amount+$t_amount;
-        $sql1="insert into passbook".$reciever_id." values('','$date','$r_name','$r_branch','$r_ifsc','$t_amount','0','$r_total','BY $s_name')";
+        $r_total=$trans->getAmount_r() + $trans->getAmount_t();
+        $sql1="insert into passbook".$trans->getRecieverid()." values('','".$trans->getDate()."','".$trans->getName_r()."','".$trans->getBranch_r()."','".$trans->getIfsc_r()."','".$trans->getAmount_r()."','0','$r_total', 'BY ".$trans->getName_r()."')";
         $mysql->query($sql1) or die($mysql->error());
         
         //insert statement into sender passbook.
-        $s_total=$s_amount-$t_amount;
-        $sql2="insert into passbook".$sender_id." values('','$date','$s_name','$s_branch','$s_ifsc','0','$t_amount','$s_total','TO $r_name')";
+        $s_total=$trans->getAmount_s() - $trans->getAmount_t();
+        $sql2="insert into passbook".$trans->getSenderid()." values('','".$trans->getDate()."','".$trans->getName_s()."','".$trans->getBranch_s()."','".$trans->getIfsc_s()."','0','".$trans->getAmount_t()."','$s_total','TO ".$trans->getName_r()."')";
          $mysql->query($sql2) or die($mysql->error());
         
         echo '<script>alert("Transfer Successful.");';
